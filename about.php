431bea0c86278e0b6ce5c9c636ad52c6fe2ba43c
ToothyDev <chosenfate@gmx.de>
2023-05-26 21:31:30 +0200

    Make code readable

--- about.php
+++ about.php
@@ -4,17 +4,15 @@
 
 <head>
     <title>About Atomic Forum</title>
-    <link rel="stylesheet" href="styling/index.css">
+    <link rel="stylesheet" href="styling/about.css">
 </head>
 <?php include_once 'scripts/php/discordusers.php' ?>
 
 <body>
-    <img src="<?php
-                echo getUser(341988909363757057)['avatar'];
-                ?>">
-    <header><?php
-            echo getUser(341988909363757057)['name'];
-            ?></header>
+    <img class=userimage src="<?php echo getUser(341988909363757057)['avatar'] ?>">
+    <header><?php echo getUser(341988909363757057)['name']; ?></header>
+    <img class=userimage src="<?php echo getUser(839237573595365406)['avatar'] ?>">
+    <header><?php echo getUser(839237573595365406)['name'] ?></header>
 </body>
 
 </html>
