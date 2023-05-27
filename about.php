1213f24cf511a1bc5cd55ff4bfd90af682813e22
ToothyDev <chosenfate@gmx.de>
2023-05-27 21:55:24 +0200

    Order team members correctly

--- about.php
+++ about.php
@@ -33,16 +33,16 @@
     <h2>Our Team</h2>
     <div class="ribbon">
         <div class="user">
-            <img class="userimage" src="<?php echo getUser(341988909363757057)['avatar'] ?>">
-            <span class="name"><?php echo getUser(341988909363757057)['name']; ?></header>
-        </div>
-        <div class="user">
             <img class="userimage" src="<?php echo getUser(839237573595365406)['avatar'] ?>">
             <span class="name"><?php echo getUser(839237573595365406)['name'] ?></header>
         </div>
         <div class="user">
             <img class="userimage" src="<?php echo getUser(513072262409355274)['avatar'] ?>">
             <span class="name"><?php echo getUser(513072262409355274)['name'] ?></header>
+        </div>
+        <div class="user">
+            <img class="userimage" src="<?php echo getUser(341988909363757057)['avatar'] ?>">
+            <span class="name"><?php echo getUser(341988909363757057)['name']; ?></header>
         </div>
         <div class="user">
             <img class="userimage" src="<?php echo getUser(104933285506908160)['avatar'] ?>">
@@ -59,6 +59,10 @@
         <div class="user">
             <img class="userimage" src="<?php echo getUser(912852147392102421)['avatar'] ?>">
             <span class="name"><?php echo getUser(912852147392102421)['name'] ?></header>
+        </div>
+        <div class="user">
+            <img class="userimage" src="<?php echo getUser(584672380085862407)['avatar'] ?>">
+            <span class="name"><?php echo getUser(584672380085862407)['name'] ?></header>
         </div>
     </div>
 </body>
